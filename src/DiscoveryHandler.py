46627d50531df150dbefd5f6c9509b780e8bc130
dewlytg <dewly_tg@163.com>
2018-09-10 11:53:47 +0800

    temp

--- src/DiscoveryHandler.py
+++ src/DiscoveryHandler.py
@@ -42,7 +42,7 @@ def getProcessList():
             command_output_all = command_output_all.strip()
             command_output_all_list = command_output_all.split("\n")
         ret = _redisHandler(discovery_process_key, command_output_all_list, "{#PROCESS}") # thought redishandler to  union last vnode list and new vnode list
-        if ret:push_metric_to_falcon(ret)
+        # if ret:push_metric_to_falcon(ret)
     except Exception as e:
         traceback.print_exc()
         print(e)
